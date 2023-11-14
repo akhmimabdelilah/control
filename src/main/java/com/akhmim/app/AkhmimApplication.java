12330594caa7b385a013368a2353f9419e4a89ae
BrightProgrammer7 <e-akhmim.a@ucd.ma>
2023-11-14 22:28:05 +0000

    control

---
+++ src/main/java/com/akhmim/app/AkhmimApplication.java
@@ -0,0 +1,13 @@
+package com.akhmim.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkhmimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkhmimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhmim/app/AkhmimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhmim.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkhmimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
